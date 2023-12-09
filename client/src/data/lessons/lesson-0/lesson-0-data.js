90f5dca46076ce1ad7c535823c430cd88d5159e3
PranshuJaiswal <2021ucs0107@iitjammu.ac.in>
2023-12-10 02:04:38 +0530

    Added MCQ

---
+++ client/src/data/lessons/lesson-0/lesson-0-data.js
@@ -0,0 +1,90 @@
+const lesson_0_data = {
+  lessonNumber: 1,
+  lessonPages: '',
+  lessonTitle: 'Javascript',
+  lessonUrl: 'Skill-1',
+  lessonUnits: [
+    {
+      unitTitle: 'MCQ',
+      unitUrl: 'MCQ',
+      unitContent: [
+        // { character: '', meanings: '' },
+        {
+          question: 'Which array method is used to add elements to the end of an array?',
+          options: [
+            { text: 'push()', isCorrect: true },
+            { text: 'pop()', isCorrect: false },
+            { text: 'shift()', isCorrect: false },
+            { text: 'unshift()', isCorrect: false },
+          ],
+        },
+        {
+          question: 'Which of the following is a correct way to declare a JavaScript object?',
+          options: [
+            { text: 'const obj = ();', isCorrect: false },
+            { text: 'let obj = {};', isCorrect: true },
+            { text: 'var obj = [];', isCorrect: false },
+            { text: 'const obj = [];', isCorrect: false },
+          ],
+        },
+        {
+          question: 'What does the "=== " operator do in JavaScript?',
+          options: [
+            { text: 'Checks for equality and type', isCorrect: true },
+            { text: 'Checks for equality only', isCorrect: false },
+            { text: 'Checks for type only', isCorrect: false },
+            { text: 'Assigns a value', isCorrect: false },
+          ],
+        },
+        {
+          question: 'How do you properly comment a single line of JavaScript code?',
+          options: [
+            { text: '// This is a comment', isCorrect: true },
+            { text: '/* This is a comment */', isCorrect: false },
+            { text: '# This is a comment', isCorrect: false },
+            { text: '-- This is a comment', isCorrect: false },
+          ],
+        },
+        {
+          question: 'What is the purpose of the "let" keyword in JavaScript?',
+          options: [
+            { text: 'Declares a constant variable', isCorrect: false },
+            { text: 'Declares a block-scoped variable', isCorrect: true },
+            { text: 'Declares a global variable', isCorrect: false },
+            { text: 'Declares a function', isCorrect: false },
+          ],
+        },
+        {
+          question: 'Which event is triggered when a user clicks on an HTML element?',
+          options: [
+            { text: 'onhover', isCorrect: false },
+            { text: 'onload', isCorrect: false },
+            { text: 'onclick', isCorrect: true },
+            { text: 'onchange', isCorrect: false },
+          ],
+        },
+        {
+          question: 'What does the "typeof" operator return for an array?',
+          options: [
+            { text: 'array', isCorrect: false },
+            { text: 'object', isCorrect: true },
+            { text: 'string', isCorrect: false },
+            { text: 'number', isCorrect: false },
+          ],
+        },
+        {
+          question: 'How can you convert a string to a number in JavaScript?',
+          options: [
+            { text: 'parseInt()', isCorrect: true },
+            { text: 'stringToNumber()', isCorrect: false },
+            { text: 'Number()', isCorrect: false },
+            { text: 'convertToNumber()', isCorrect: false },
+          ],
+        },
+      ],
+    },
+   ,
+  ],
+};
+
+export default lesson_0_data;
